99402232766fafd9b8d1c81d17e883bc1961f37d
Bohdan Solovey <30517042+bohdan462@users.noreply.github.com>
2024-12-11 17:06:20 -0600

    Initial Commit

---
+++ Explore Recipes/Explore_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explore_RecipesApp.swift
+//  Explore Recipes
+//
+//  Created by Bohdan Tkachenko on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Explore_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
